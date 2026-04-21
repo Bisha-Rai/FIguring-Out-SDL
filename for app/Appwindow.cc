b404d1ed0dcab493ec43b420b1eb400d17d6a928
Bisha-Rai <bishalchamling14@gmail.com>
2026-04-21 16:39:18 +0545

    Widnow stays

--- for app/Appwindow.cc
+++ for app/Appwindow.cc
@@ -4,11 +4,19 @@
 
     SDL_Window*window = NULL;
     SDL_Renderer*renderer=NULL;
+     bool run=false;
 int main(int argc,char* argv[])
 {
-    SDL_Window*window = NULL;
-    SDL_Renderer*renderer=NULL;
-    window=SDL_CreateWindow("APPLICATION",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1000,500,SDL_WINDOW_SHOWN);
+   if(SDL_Init(SDL_INIT_EVERYTHING)==0)
+   {
+    window=SDL_CreateWindow("Application",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1000,500,SDL_WINDOW_SHOWN);
     renderer=SDL_CreateRenderer(window,-1,0);
+    run=true;
+
+   }
+    while(run)
+   {
+        
+   }
     return 0;
 }
